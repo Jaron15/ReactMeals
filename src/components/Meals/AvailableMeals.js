00eda0baf77534b072fd79f7d4feb576abc6308d
Jaron <jjkenyon777@gmail.com>
2022-06-26 11:37:56 -0700

    added Card UI component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
 
 const DUMMY_MEALS = [
     {
@@ -33,7 +34,9 @@ const AvailableMeals = () => {
 
     return (
       <section className={classes.meals}>
+        <Card>
         <ul>{mealsList}</ul>
+        </Card>
       </section>
     );
 
