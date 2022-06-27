1b13c015b205b309ae47eedfe1ebc3d8a38a689a
Jaron <jjkenyon777@gmail.com>
2022-06-26 17:33:22 -0700

    MealItemForm/Input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 
@@ -12,7 +13,7 @@ const price = `$${props.price.toFixed(2)}`;
           <div className={classes.price}>{price}</div>
         </div>
         <div>
-
+        <MealItemForm id={props.id}/>
         </div>
       </li>
     );
